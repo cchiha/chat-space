7a1bdf70087bb81d4a1c57163697f530292323b3
cchiha <paokun59@gmail.com>
2018-09-07 18:24:16 +0900

    Edit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,19 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
   }
 
-  config.fog_directory  = 'chat-space-cchiha'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-cchiha'
+    case Rails.env
+    when 'development'
+        config.fog_directory  = ENV['AWS_BUCKET_NAME']
+        config.asset_host = ENV['AWS_BUCKET_URL']
+    when 'production'
+        config.fog_directory  = ENV['AWS_BUCKET_NAME']
+        config.asset_host = ENV['AWS_BUCKET_URL']
+    end
 end
